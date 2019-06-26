980200f063dbe062181145d64c3ec99ef74a8caa
abing <abing@gcores.com>
2019-06-26 14:33:17 +0800

    fix import error

--- android/app/src/main/java/com/akulyk/react/androidwebview/WebViewManager.java
+++ android/app/src/main/java/com/akulyk/react/androidwebview/WebViewManager.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import android.graphics.Bitmap;
 import android.os.Build;
 import android.text.TextUtils;
+import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.webkit.ConsoleMessage;
 import android.webkit.GeolocationPermissions;
